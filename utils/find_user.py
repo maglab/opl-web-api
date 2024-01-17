f22af731b6365c7a81f2c718431eaf91c554cdd7
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-01-17 13:38:11 +0000

    Checked for if contact is None before creating the contact.

--- utils/find_user.py
+++ utils/find_user.py
@@ -9,21 +9,26 @@ from open_problems.models.open_problems import SubmittedProblems, OpenProblems
 def find_user(
     data: dict, open_problem_object: Type[SubmittedProblems | OpenProblems]
 ) -> object:
-    organisation = Organisation.objects.get_or_create(
-        info_title=data["organisation"].lower()
-    )
+    organisation = Organisation.objects.get_or_create(info_title=data["organisation"])
     contact_query = Q(
         first_name=data["first_name"], last_name=data["last_name"], email=data["email"]
     )
     contact = Contact.objects.filter(contact_query).first()
-    if contact.exists:
+    # Try to find an existing Contact
+    contact = Contact.objects.filter(contact_query).first()
+
+    if contact is not None:
         if not contact.organisation:
             contact.organisation = organisation
+        # Save the Contact
+        contact.save()
         return contact
     else:
-        contact = Contact(
+        # Create a new Contact and save it
+        new_contact = Contact(
             first_name=data["first_name"],
             last_name=data["last_name"],
             organisation=organisation,
         )
-        return contact
+        new_contact.save()
+        return new_contact
