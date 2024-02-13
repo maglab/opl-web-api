e5d445519a6af71545ea1f8bb7980cbad80abecb
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-13 13:45:51 +0000

    Update urls.py
    Moved new view into urls

--- open_problems/urls.py
+++ open_problems/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views.open_problems_views import RetrieveProblems, RetrieveSingleProblem
 from .views.references_views import ListReferencesView
-from .views.submitted_problems_views import SubmitOpenProblem
+from .views.submitted_problems_views import SubmitOpenProblemView
 from .views.utils import test_view
 from .views.utils import verify_token
 
@@ -13,7 +13,7 @@ urlpatterns = [
     # Single problem
     path("<int:id>", RetrieveSingleProblem.as_view()),
     # User submitted problem view
-    path("submit", SubmitOpenProblem.as_view()),
+    path("submit", SubmitOpenProblemView.as_view()),
     # Verify token for recaptcha
     path("verify-token", verify_token),
     # Get references for a problem
