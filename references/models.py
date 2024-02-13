e4e158761c4f12f86d9571710f1ceff38c453275
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-13 17:37:15 +0000

    Update models.py

--- references/models.py
+++ references/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     author_id = models.AutoField(primary_key=True)
-    author_name = models.CharField(max_length=50)
+    author_name = models.CharField(max_length=50, unique=True)
 
     def __str__(self) -> str:
         return f"{self.author_id}: {self.author_name}"
@@ -11,7 +11,7 @@ class Author(models.Model):
 
 class Journal(models.Model):
     journal_id = models.AutoField(primary_key=True)
-    journal_name = models.CharField(max_length=100)
+    journal_name = models.CharField(max_length=100, unique=True)
 
     class Meta:
         db_table_comment = "Contains the ids and names of article journals"
@@ -36,10 +36,10 @@ class RefType(models.Model):
 class Reference(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(
-        max_length=150
+        max_length=150, unique=True
     )  # max_length changed from 100 to 150 by Hamid
-    citation = models.TextField()
-    doi = models.CharField(max_length=100, null=True, blank=True)
+    citation = models.TextField(max_length=500)
+    doi = models.CharField(max_length=100, null=True, blank=True, unique=True)
     relevance = models.PositiveSmallIntegerField(null=True, blank=True)
     year = models.CharField(max_length=4)
     isbn = models.IntegerField(max_length=15, null=True, blank=True)
