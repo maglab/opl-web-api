8e37d80442fcaa1523e6b834c6b552b63badff7a
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-13 13:44:57 +0000

    Updated imports

--- open_problems/serializers.py
+++ open_problems/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
+from open_problems.models.open_problems import (
+    Contact,
+    SubmittedOpenProblem,
+)
 from open_problems.models.open_problems import OpenProblem
+from utils.base_serializer import BaseSerializer
 from utils.recursive_serializer import RecursiveSerializer
 
 
@@ -24,3 +29,21 @@ class OpenProblemsSerializer(serializers.ModelSerializer):
     @staticmethod
     def get_post_count(obj):
         return obj.post_set.count()
+
+
+# Serializer for parent node of open problem
+class ParentSerializer(BaseSerializer):
+    class Meta:
+        model = OpenProblem
+
+
+class ContactSerializer(BaseSerializer):
+    class Meta:
+        model = Contact
+
+
+# Serializer for user submitted open problems
+class SubmittedOpenProblemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubmittedOpenProblem
+        fields = "__all__"
