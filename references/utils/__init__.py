ed3fb72f974340dfc422c82e08f43c4f30c2ed36
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-13 13:44:04 +0000

    Created classes for DOI and PMID conversion.

---
+++ references/utils/__init__.py
@@ -0,0 +1,3 @@
+from .retrieve_reference import DoiConverter, PmidConverter, PMIDRequestException
+
+__all__ = ["DoiConverter", "PmidConverter", "PMIDRequestException"]
--- references/views.py
+++ references/views.py
@@ -1,27 +1,2 @@
-from django.db import models
-from rest_framework.generics import ListAPIView
-
-from .models import Reference
-from .serializers import ReferenceSerializer
 
 
-# Create your views here.
-
-
-class ListReferencesView(ListAPIView):
-    serializer_class = ReferenceSerializer
-
-    def get_queryset(self):
-        model_name = self.kwargs["model"]
-        model_class = getattr(models, model_name, None)
-        if model_class is None:
-            # Handle case where model is not found
-            return Reference.objects.none()
-
-        instance_id = self.kwargs["pk"]
-        try:
-            instance = model_class.objects.get(pk=instance_id)
-            return instance.references.all()
-        except model_class.DoesNotExist:
-            # Handle case where instance is not found
-            return Reference.objects.none()
