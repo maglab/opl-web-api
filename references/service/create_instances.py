131c9960724660636af118134abea3fdd71007f0
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-17 11:43:17 +0000

    Delete create_instances.py

--- references/service/create_instances.py
+++
@@ -1,9 +0,0 @@
-from ..models import Reference
-
-
-def create_reference_instance(reference_dictionary: dict):
-    # The output of the doi / pmid converters should mirror the model fields
-    reference_instance, created = Reference.objects.get_or_create(
-        **reference_dictionary
-    )
-    return reference_instance
