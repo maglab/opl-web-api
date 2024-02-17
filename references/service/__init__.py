651d1c5394900932f873a3451df8c552459fc714
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-17 11:43:23 +0000

    Update __init__.py

--- references/service/__init__.py
+++ references/service/__init__.py
@@ -1,9 +1,7 @@
-from .create_instances import create_reference_instance
 from .id_converter import DoiConverter, PmidConverter, PMIDRequestException
 
 __all__ = [
     "DoiConverter",
     "PmidConverter",
     "PMIDRequestException",
-    "create_reference_instance",
 ]
