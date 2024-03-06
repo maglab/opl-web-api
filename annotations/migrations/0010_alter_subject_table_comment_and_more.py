b51c2847eb91905293906f0f5dc7af0d14f2467d
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-06 11:28:07 +0000

    Update 0010_alter_subject_table_comment_and_more.py

--- annotations/migrations/0010_alter_subject_table_comment_and_more.py
+++ annotations/migrations/0010_alter_subject_table_comment_and_more.py
@@ -40,7 +40,7 @@ class Migration(migrations.Migration):
                 on_delete=django.db.models.deletion.DO_NOTHING,
                 primary_key=True,
                 serialize=False,
-                to="references.reference",
+                to="open_problems.reference",
             ),
         ),
         migrations.AlterField(
