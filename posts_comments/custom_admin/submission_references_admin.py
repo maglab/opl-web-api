56ef57730063b79ccbefe9699788d266570cc3be
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-15 14:56:31 +0000

    Do not need these files

--- posts_comments/custom_admin/submission_references_admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from posts_comments.models.Post import PostReferences
-
-
-# Customm admin class for Submissions and their references
-class SubmissionReferenceAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in PostReferences._meta.fields]
