74fc2ef98516cc62c3df6fd88f7d41ef043c6ada
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-15 14:58:42 +0000

    Update urls.py

--- posts_comments/urls.py
+++ posts_comments/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
 
-from posts_comments.views.comment_view import (
-    post_comment,
-    ListComments,
-    CommentDetail
-)
+from posts_comments.views.comment_view import post_comment, ListComments, CommentDetail
 from posts_comments.views.posts_view import (
-    get_references,
     ListPosts,
     PostDetail,
-    SubmitPost,
 )
 from posts_comments.views.verify_references_view import (
     verify_reference,
@@ -20,14 +14,11 @@ from posts_comments.views.verify_references_view import (
 urlpatterns = [
     path("all", ListPosts.as_view()),
     path("<int:id>/", ListPosts.as_view(), name="list-submissions-by-open-problem"),
-    path("<int:id>/submit", SubmitPost.as_view()),
+    # path("<int:id>/submit", SubmitPost.as_view()),
     path("get/<int:id>", PostDetail.as_view()),
     path("get/<int:id>/comments", ListComments.as_view()),
     path("get/<int:post_id>/<int:comment_id>", CommentDetail.as_view()),
     path("post/<int:post_id>/comment/submit", post_comment),
     path("verify-reference", verify_reference),
     path("verify-references", verify_references),
-    path(
-        "get/<int:id>/submission/reference", get_references
-    ),  # get references for a particular solution submision
 ]
