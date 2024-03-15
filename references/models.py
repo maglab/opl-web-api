a9d1abb0e76d8ad6b6ff89acd787bdb4b18f165f
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-15 14:55:01 +0000

    Update models.py

--- references/models.py
+++ references/models.py
@@ -11,7 +11,7 @@ class Author(models.Model):
 
 class Journal(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     class Meta:
         db_table_comment = "Contains the ids and names of article journals"
