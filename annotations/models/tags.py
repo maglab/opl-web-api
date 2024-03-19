9120203202a2224b853eb4dda9374da697014972
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-19 12:44:27 +0000

    Increased max length of tag title

--- annotations/models/tags.py
+++ annotations/models/tags.py
@@ -7,7 +7,7 @@ from .annotations import AnnotationsProblems
 class Tag(models.Model):
     id = models.AutoField(primary_key=True)  # Field name made lowercase.
     title = models.CharField(
-        max_length=40, blank=True, null=True
+        max_length=100, blank=True, null=True
     )  # Field name made lowercase.
     description = models.TextField(blank=True, null=True)  # Field name made lowercase.
     parent = models.ForeignKey(
