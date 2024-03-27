9df79ff7a5dbd6c839976e81726774d15474a2dd
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-27 13:42:13 +0000

    Extend the user model to create a user profile for additional user fields.

--- users/models.py
+++ users/models.py
@@ -1,14 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
 
-
-# Make one to one copy or change field names ??
-class User(AbstractUser):
-    firebase_uid = models.CharField(max_length=50, blank=True, null=True)
-    job_role = models.CharField(max_length=100, blank=True, null=True)
+# Need to test how this userprofile would work with open Id.
+class UserProfile(models.Model):
+    account = models.OneToOneField(
+        User, on_delete=models.CASCADE, blank=False, null=False
+    )
+    firebase_uuid = models.CharField(max_length=50, blank=True, null=True)
     affiliation = models.CharField(max_length=100, blank=True, null=True)
+    role = models.CharField(max_length=100, blank=True, null=True)
 
-    class Meta:
-        db_table = "auth_user"
+    def __str__(self):
+        return self.account.username
+
+
+# Signal to save or update the user profile whenever the User instance is updated. May not need for now.
+# @receiver(post_save, sender=User)
+# def update_user_profile(sender, instance, **kwargs):
+#     UserProfile.objects.get_or_create(account=instance)
+#     instance.userprofile.save()
