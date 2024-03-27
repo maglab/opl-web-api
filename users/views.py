4bd7f1b8c04d3a1f5f429fe05d248fccf4469a1a
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-27 13:40:48 +0000

    Simple registeration view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import UserRegistrationSerializer
+import mailersend
 
-# Create your views here.
+
+class UserRegistrationView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        if self.serializer_class.is_valid():
+            self.serializer_class.save()
