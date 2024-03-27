07ecd9718c30a660cc332ec9ab73fe40be2c7648
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-27 13:42:32 +0000

    Serializer for User registeration and user profile

---
+++ users/serializers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ["firebase_uuid", "role", "affiliation"]
+
+
+# This serializer creates the user profile at the end if it is populated.
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(required=False)
+
+    class Meta:
+        model = User
+        fields = ["username", "password", "email", "profile"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop("profile", None)
+        user = User.objects.create_user(**validated_data)
+        if profile_data:
+            UserProfile.objects.get_or_create(account=user, **profile_data)
+        return user
