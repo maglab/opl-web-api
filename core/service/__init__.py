87c970b75cc60b5e58dc025f7c9eae6c401f6e68
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-04-30 17:19:56 +0100

    Implemented mailer class to be used with templates

--- core/service/__init__.py
+++ core/service/__init__.py
@@ -1,4 +1,10 @@
 from .service import get_or_create_instances
 from .queryset_helpers import *
+from .mailer import MailTrapService
 
-__all__ = ["get_or_create_instances", "get_queryset_annotated", "get_queryset_ordered"]
+__all__ = [
+    "get_or_create_instances",
+    "get_queryset_annotated",
+    "get_queryset_ordered",
+    "MailTrapService",
+]
