f17dccf4edb560ceb62424b5c16192c6a927b538
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-05-02 09:34:59 +0100

    Update admin.py

--- open_problems/admin.py
+++ open_problems/admin.py
@@ -12,7 +12,7 @@ class OpenProblemAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     resource_class = OpenProblemResource
     list_display = ["title", "problem_id", "contact", "is_active", "parent_problem"]
     actions = ["toggle_active_status"]
-    search_fields = ["id", "title"]
+    search_fields = ["problem_id", "title"]
     list_filter = ["is_active", "parent_problem"]
     autocomplete_fields = ["tags"]
 
@@ -42,6 +42,10 @@ class SubmittedProblemsAdmin(admin.ModelAdmin):
                 parent_problem=submitted_problem.parent_problem,
                 contact=submitted_problem.contact,
                 references=submitted_problem.references,
+                tags=submitted_problem.tags,
+                genes=submitted_problem.genes,
+                species=submitted_problem.species,
+                compounds=submitted_problem.compounds,
             )
             # Save the problem
             open_problem.save()
